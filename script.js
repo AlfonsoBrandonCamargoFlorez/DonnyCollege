32ef722200c66bc9f27928b5344f6294431e05ab
AlfonsoBrandonCamargoFlorez <b.c.florez@hotmail.com>
2024-11-13 21:16:56 -0500

    Se realizan pequenas mejoras de compatibilidad

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+// script.js
 
+let currentIndex = 0;
+const images = document.querySelectorAll('.carousel-images .carousel-slide');
+const totalImages = images.length;
 
-
-
-
+function showSlide(index) {
+    if (index >= totalImages) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalImages - 1;
+    } else {
+        currentIndex = index;
+    }
+    const offset = -currentIndex * 100;
+    document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
+}
 
 function moveSlide(step) {
     showSlide(currentIndex + step);
